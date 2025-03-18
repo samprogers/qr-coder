bc3ff23d9fdeacb36dbc8a5723d1d07b87eeb7ad
Sam Rogers <samuelpaulrogers@gmail.com>
2025-03-18 16:18:04 -0400

    more tests

--- tests/test_qr.py
+++ tests/test_qr.py
@@ -5,3 +5,13 @@ def test_get_url_qr():
     client = Client()
     response = client.get("/api/generate?url=https://www.google.com")
     assert response.status_code == 200
+
+def test_privacy():
+    client = Client()
+    response = client.get("/privacy")
+    assert response.status_code == 200
+
+def test_cookies():
+    client = Client()
+    response = client.get("/cookies")
+    assert response.status_code == 200
