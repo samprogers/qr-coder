32e4422ee1366dc320713321bc536190e5f72924
Sam Rogers <samuelpaulrogers@gmail.com>
2025-03-22 15:16:19 -0400

    add robots url

--- ui/urls.py
+++ ui/urls.py
@@ -4,6 +4,7 @@ from ui import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("robots.txt", views.robots, name="robots"),
     path("privacy", views.privacy, name="privacy"),
     path("cookies", views.cookies, name="cookies")
 ]
